0abe8ac29d654dbb067aa350eb86c056952e0d2f
Thibault Carmantrand <t.carmantrand@openmailbox.org>
2017-03-21 15:28:36 +0100

    Added main file : Dactyle.js

---
+++ Dactyle.js
@@ -0,0 +1,52 @@
+class Dactyle {
+
+    constructor (target, text, minSpeed, maxSpeed, htmlSupport) {
+        this.target = target;
+        this.text = text;
+        this.table = this.setTable(this.text);
+        this.minSpeed = minSpeed;
+        this.maxSpeed = maxSpeed;
+        this.iteration = 0;
+        this.htmlSupport = htmlSupport || true;
+    }
+
+    type () {
+        let timeoutID = setTimeout(_ => {
+            if (this.htmlSupport) {
+                if (this.table[this.iteration].endsWith('<')) {
+                    do {
+                        this.iteration++;
+                    } while (!this.table[this.iteration].endsWith('>'));
+                }
+                this.target.innerHTML = this.table[this.iteration];
+            } else {
+                this.target.textContent = this.table[this.iteration];
+            }
+
+            this.iteration++;
+            if (this.iteration === this.table.length) {
+                clearTimeout(timeoutID);
+            } else {
+                this.type();
+            }
+        }, this.getRandomSpeed());
+    }
+
+
+
+    setTable (text) {
+        let table = [];
+        for (let i = 0; i < text.length; i++) {
+            table.push(text.substring(0, i+1));
+        }
+        return table;
+    }
+
+    getRandomSpeed() {
+        return Math.floor(Math.random() * (this.maxSpeed - this.minSpeed)) + this.minSpeed;
+    }
+
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+}
